91ff8e75c587ac12b52762b714ab5295c4e98e4d
Mrafihaidar <90681157+Mrafihaidar@users.noreply.github.com>
2021-11-30 23:42:43 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class menu extends CI_Controller 
+{
+    
+    public function index()
+    {
+        $data['title'] = 'menu management';
+        $data['user'] = $this->db->get_where('user', ['email' => 
+        $this->session->userdata('email')])->row_array();
+        
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('menu/index', $data);
+        $this->load->view('templates/footer');
+    }
+}
